d18ff881f2fa59983d195146bd961ab034f2b7b7
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-10 09:05:47 +0200

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -1,7 +1,7 @@
 import http.client
 def titaniumcruciblefaas(req):
-    connection =  httplib.HTTPConnection('35.201.95.66:80')
+    #connection =  httplib.HTTPConnection('35.201.95.66:80')
     body_content = 'BODY CONTENT GOES HERE'
-    connection.request('PUT', '/', body_content)
-    result = connection.getresponse()
+    #connection.request('PUT', '/', body_content)
+    #result = connection.getresponse()
     return {'result': 'ok'}                                 
