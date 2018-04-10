36b2df82be2ab1ed42752fd21beef896a8d0f9da
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-10 08:19:57 +0200

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -4,4 +4,4 @@ def titaniumcruciblefaas(req):
     params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
     f = urllib.urlopen("35.201.95.66", params)
     print f.read( )
-    return jsonify({'result': 'ok'})
+    return {'result': 'ok'}
