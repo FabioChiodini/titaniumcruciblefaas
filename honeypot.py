c72191393eb6d5d658ddf2304ed0d5012fe115c1
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-10 07:48:35 +0200

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -1,6 +1,6 @@
 # from flask import Flask, jsonify, request
 import urllib
-def log_request(req):
+def titaniumcruciblefaas(req):
     params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
     f = urllib.urlopen("35.201.95.66", params)
     return jsonify({'result': 'ok'})
