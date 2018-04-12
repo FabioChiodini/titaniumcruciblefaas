144ad73c458ce1eecc446dafa6939871979e5b39
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-12 17:18:52 +0200

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -1,7 +1,38 @@
-import http.client
+# from flask import Flask, jsonify, request
+import os
+import logging
+import logstash
+import sys
+import logging.handlers
+
+#if 'LOG_HOST' not in os.environ:
+#    raise(Exception("LOG_HOST NOT DEFINED"))
+
+#host = os.environ['LOG_HOST']
+
+test_logger = logging.getLogger('python-http-logger')
+test_logger.setLevel(logging.INFO)
+#test_logger.addHandler(logging.handlers.HTTPHandler(host, '/', method='POST'))
+test_logger.addHandler(logging.handlers.HTTPHandler('35.190.36.51', '/', method='POST'))
+
+#app = Flask(__name__)
+
 def titaniumcruciblefaas(req):
-    #connection =  httplib.HTTPConnection('35.201.95.66:80')
-    body_content = 'BODY CONTENT GOES HERE'
-    #connection.request('PUT', '/', body_content)
-    #result = connection.getresponse()
-    return {'result': 'ok'}                                 
+    extra = {
+        'ip': request.environ.get('X-Forwarded-For', request.remote_addr),
+        'url': req.full_path,
+    }
+    test_logger.info('honeypot: ', extra=extra)
+    #new line
+    return {'extra': extra}
+
+#    data_to_log.update(req.headers)
+
+#@app.route('/', defaults={'path': ''})
+#@app.route('/<path:path>')
+#def honey(path):
+#    log_request(request)
+#    return jsonify({'result': 'ok'})
+
+#if __name__ == '__main__':
+#app.run(host="0.0.0.0",port=8080)
